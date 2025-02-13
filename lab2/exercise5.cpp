03dfd2a53f3f3b0cfa76475350b968f607e5a95c
tuvshinerdene1 <tuvshin.erdene25@gmail.com>
2025-02-13 20:53:15 +0800

    started lab 2

---
+++ lab2/exercise5.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char *p1;
+    int *p2;
+    double *p3;
+    cout << sizeof(p1)<<endl<<sizeof(p2)<<endl<<sizeof(p3)<<endl;
+    return 0;
+}
